7efae906a12ac5d0aee4a84e3f34b850772a1bf7
Guillaume Van Wassenhove <Guillaume.VanWassenhove@UGent.be>
2020-02-21 13:31:24 +0100

    Fix error in checkpoint callback

--- train_nn.py
+++ train_nn.py
@@ -1,9 +1,9 @@
 from pytorch_lightning import Trainer
-from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.logging import TensorBoardLogger
 from pytorch_lightning.callbacks import ModelCheckpoint
 from model.ehnet_model import EHNetModel
 from argparse import Namespace
-from os import getcwd
+import os
 
 train_dir = './WAVs/dataset/training'
 val_dir = './WAVs/dataset/validation'
@@ -21,8 +21,9 @@ model = EHNetModel(hparams=Namespace(**{'train_dir': train_dir,
                                         'n_lstm_units': 1024,
                                         'lstm_dropout': 0.3}))
 
-logger = TensorBoardLogger(save_dir=getcwd(), name="lightning_logs")
-checkpoint_callback = ModelCheckpoint(filepath=logger.save_dir, verbose=1, save_top_k=5, mode='min')
+logger = TensorBoardLogger(save_dir=os.getcwd(), name="lightning_logs")
+checkpoint_path = os.path.join(logger.save_dir, logger.name, f"version_{logger.version}")
+checkpoint_callback = ModelCheckpoint(filepath=checkpoint_path, verbose=1, save_top_k=5, mode='min')
 
 trainer = Trainer(gpus=1, min_epochs=200, logger=logger, checkpoint_callback=checkpoint_callback)
 trainer.fit(model)
