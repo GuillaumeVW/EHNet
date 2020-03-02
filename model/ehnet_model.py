608357875c04d871bba47bba0e1fff45321adf71
Guillaume Van Wassenhove <Guillaume.VanWassenhove@UGent.be>
2020-03-02 12:11:26 +0100

    Fix typo

--- model/ehnet_model.py
+++ model/ehnet_model.py
@@ -179,7 +179,7 @@ class EHNetModel(pl.LightningModule):
     def __dataloader(self, train):
         # init data generators
 
-        transform = Spectrogram(n_fft=(self.n_frequency_bins - 1) * 2, normal=True)
+        transform = Spectrogram(n_fft=(self.n_frequency_bins - 1) * 2, normalized=True)
 
         if train:
             dataset = WAVDataset(self.train_dir, transform=transform)
