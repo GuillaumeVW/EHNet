c7c6252f3e1f1cf974547bdbf3faef7776249d58
Guillaume Van Wassenhove <Guillaume.VanWassenhove@UGent.be>
2020-03-13 17:52:58 +0100

    Switch to Comet.NL logging

--- train_nn.py
+++ train_nn.py
@@ -1,5 +1,5 @@
 from pytorch_lightning import Trainer
-from pytorch_lightning.logging import TensorBoardLogger
+from pytorch_lightning.logging import CometLogger
 from pytorch_lightning.callbacks import ModelCheckpoint
 from model.ehnet_model import EHNetModel
 from argparse import Namespace
@@ -10,7 +10,7 @@ val_dir = './WAVs/dataset/validation'
 
 model = EHNetModel(hparams=Namespace(**{'train_dir': train_dir,
                                         'val_dir': val_dir,
-                                        'batch_size': 32,
+                                        'batch_size': 16,
                                         'n_frequency_bins': 256,
                                         'n_kernels': 256,
                                         'kernel_size_f': 32,
@@ -19,10 +19,9 @@ model = EHNetModel(hparams=Namespace(**{'train_dir': train_dir,
                                         'n_lstm_units': 1024,
                                         'lstm_dropout': 0.2}))
 
-logger = TensorBoardLogger(save_dir=os.getcwd(), name="lightning_logs")
-checkpoint_path = os.path.join(logger.save_dir, logger.name, f"version_{logger.version}", "checkpoints")
+logger = CometLogger(api_key="4iSR8MNOiJzKARKC4OTTa7kg8", project_name="ehnet", workspace="guillaumevw")
+checkpoint_path = os.path.join('lightning_logs', str(logger.version))
 os.makedirs(checkpoint_path)
-checkpoint_callback = ModelCheckpoint(filepath=checkpoint_path, verbose=1, save_top_k=3, mode='min')
-
+checkpoint_callback = ModelCheckpoint(filepath=checkpoint_path, verbose=1, save_top_k=1, mode='min')
 trainer = Trainer(gpus=1, min_epochs=200, logger=logger, checkpoint_callback=checkpoint_callback)
 trainer.fit(model)
