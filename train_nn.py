453508f60e36a55c8f3a9a97772129cbbaf4e4b1
Guillaume Van Wassenhove <Guillaume.VanWassenhove@UGent.be>
2020-03-14 12:22:50 +0100

    Switch to Neptune.AI logger

--- train_nn.py
+++ train_nn.py
@@ -1,5 +1,5 @@
 from pytorch_lightning import Trainer
-from pytorch_lightning.logging import CometLogger
+from pytorch_lightning.logging import NeptuneLogger
 from pytorch_lightning.callbacks import ModelCheckpoint
 from model.ehnet_model import EHNetModel
 from argparse import Namespace
@@ -8,20 +8,25 @@ import os
 train_dir = './WAVs/dataset/training'
 val_dir = './WAVs/dataset/validation'
 
-model = EHNetModel(hparams=Namespace(**{'train_dir': train_dir,
-                                        'val_dir': val_dir,
-                                        'batch_size': 16,
-                                        'n_frequency_bins': 256,
-                                        'n_kernels': 256,
-                                        'kernel_size_f': 32,
-                                        'kernel_size_t': 11,
-                                        'n_lstm_layers': 2,
-                                        'n_lstm_units': 1024,
-                                        'lstm_dropout': 0.2}))
+hparams = {'train_dir': train_dir,
+           'val_dir': val_dir,
+           'batch_size': 64,
+           'n_frequency_bins': 256,
+           'n_kernels': 256,
+           'kernel_size_f': 32,
+           'kernel_size_t': 11,
+           'n_lstm_layers': 2,
+           'n_lstm_units': 1024,
+           'lstm_dropout': 0.2}
 
-logger = CometLogger(api_key="4iSR8MNOiJzKARKC4OTTa7kg8", project_name="ehnet", workspace="guillaumevw")
+model = EHNetModel(hparams=Namespace(**hparams))
+
+logger = NeptuneLogger(api_key="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vdWkubmVwdHVuZS5haSIsImFwaV91cmwiOiJodHRwczovL3VpLm5lcHR1bmUuYWkiLCJhcGlfa2V5IjoiYTc4YTJjN2YtMzY3NC00OWVhLTk4MTItZjBhYzY2NjEyZjg5In0=",
+                       project_name="guillaumevw/ehnet",
+                       params=hparams,
+                       upload_source_files=['train_nn.py', 'model/ehnet_model.py'])
 checkpoint_path = os.path.join('lightning_logs', str(logger.version))
 os.makedirs(checkpoint_path)
 checkpoint_callback = ModelCheckpoint(filepath=checkpoint_path, verbose=1, save_top_k=1, mode='min')
-trainer = Trainer(gpus=1, min_epochs=200, logger=logger, checkpoint_callback=checkpoint_callback)
+trainer = Trainer(gpus=1, min_epochs=200, logger=logger, checkpoint_callback=checkpoint_callback, progress_bar_refresh_rate=1)
 trainer.fit(model)
